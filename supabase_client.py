a8dbf7cfcb8fba496413ae4fd80587d80f8ba787
DadeElColosso87 <davide.pellanda87@gmail.com>
2026-05-14 14:53:06 +0200

    Update supabase_client.py

--- supabase_client.py
+++ supabase_client.py
@@ -4,7 +4,7 @@ import ssl
 # ✅ FIX SSL (compatibile con la tua versione)
 ssl._create_default_https_context = ssl._create_unverified_context
 
-url = "https://lmqbydfzqyolwsjjydye.supabase.co"
-key = "sb_publishable__Mryk_lmsCyf6OZrfxzQ1w_j4tRTgLp"
+url = "https://jxueaeulfyrtufsfhieu.supabase.co"
+key = "sb_publishable_Mryk_lmsCyf6OZrfxzQ1w_j4tRTgLp"
 
 supabase = create_client(url, key)
